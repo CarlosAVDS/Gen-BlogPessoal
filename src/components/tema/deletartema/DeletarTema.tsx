fa5bc39a0433022c112881a88cd05628f2aa8ef4
CarlosAVDS <carlosalbertovds04@gmail.com>
2026-01-22 11:12:43 -0300

    Deploy BlogPessoal

--- src/components/tema/deletartema/DeletarTema.tsx
+++ src/components/tema/deletartema/DeletarTema.tsx
@@ -4,6 +4,7 @@ import { AuthContext } from "../../../contexts/AuthContext"
 import type Tema from "../../../models/Tema"
 import { buscar, deletar } from "../../../services/Service"
 import { ClipLoader } from "react-spinners"
+import { ToastAlerta } from "../../../utils/ToastAlerta"
 
 function DeletarTema() {
 
@@ -30,7 +31,7 @@ function DeletarTema() {
     }
     useEffect(() => {
         if (token === '') {
-            alert('Você precisa estar logado')
+            ToastAlerta('Você precisa estar logado', 'erro')
             navigate('/')
         }
     }, [token])
@@ -49,13 +50,13 @@ function DeletarTema() {
                     'Authorization': token
                 }
             })
-            alert('Tema apagado com sucesso')
+            ToastAlerta('Tema apagado com sucesso', 'sucesso')
 
         } catch (error: any) {
             if (error.toString().includes('481')) {
                 handleLogout()
             } else {
-                alert('Erro ao deletar o tema.')
+                ToastAlerta('Erro ao deletar o tema.', 'erro')
             }
         }
         setIsLoading(false)
